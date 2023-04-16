09fecc218665a80f5985a94bbc39e9eda76887ff
Ivan <vankata294n4@gmail.com>
2023-04-16 17:01:24 +0300

    intial

---
+++ brainzone/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { SequinceMemory } from './components/SequinceMemory';
+
+function App() {
+  return (
+    <SequinceMemory/>
+  );
+}
+
+export default App;
---
+++ brainzone/src/components/SequinceMemory.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SequinceMemory = () => {
+  return (
+    <div>SequinceMemory</div>
+  )
+}
