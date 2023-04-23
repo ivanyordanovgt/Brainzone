2052392dc016a459b5a1344342067f8d6f79c2fb
Ivan <vankata294n4@gmail.com>
2023-04-23 17:54:11 +0300

    Added router and new hightlight audio

--- brainzone/src/App.js
+++ brainzone/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { SequinceMemory } from './components/SequinceMemory/SequinceMemory';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from './components/homePage/HomePage';
 
 function App() {
   return (
-    <SequinceMemory/>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage></HomePage>} />
+        <Route path="/sequince" element={<SequinceMemory/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
