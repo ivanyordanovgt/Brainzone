fa26216ee81fe990f7c7ae74412981a5776546fe
Ivan <vankata294n4@gmail.com>
2023-04-23 18:57:12 +0300

    Reaction time

--- brainzone/src/components/reactionTime/ReactionTime.js
+++ brainzone/src/components/reactionTime/ReactionTime.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from "./reactionTime.module.css"
+
 
 export const ReactionTime = () => {
-  return (
-    <div>ReactionTime</div>
-  )
+    const [reactionTime, setReactionTime] = useState(null);
+    const [reactState, setReactState] = useState(false);
+    const [lookForClick, setLookForClick] = useState(false);
+    const [startTime, setStartTime] = useState(0);
+    const [points, setPoints] = useState(0);
+    function onReactionDivClick() {
+        if (reactState) {
+            if (lookForClick) {
+                setPoints(p => p+1)
+                setReactionTime(new Date()-startTime)
+                setReactState(false); setLookForClick(false);
+            } else {
+                console.log('Click when you see green!');
+            };
+
+            return
+        };
+        setReactState(true)
+        setTimeout(() => {
+            const date = new Date();
+            setStartTime(date)
+            setLookForClick(true)
+        }, Math.floor(Math.random() * 5001))
+        
+    }
+
+    return (
+        <div>
+            {reactionTime ? <div>You reaction in {reactionTime}ms!</div>: ''}
+            <div onClick={onReactionDivClick} className={`${styles.reactionTimeDiv} ${ lookForClick ? styles.waitingForClick: 'nopent'}`}>
+
+            </div>
+        </div>
+    )
 }
