478843a107a94a98bbc9049dd047a036ef81193c
Ivan <vankata294n4@gmail.com>
2023-04-24 15:45:41 +0300

    Changed way of rendering target and added onClick function

--- brainzone/src/components/AimTrainer/AimTrainer.js
+++ brainzone/src/components/AimTrainer/AimTrainer.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Target } from './Target'
 
 export const AimTrainer = () => {
+
+  const [points, setPoints] = useState(false)
+  const [clickedOuterColor, setClickedOuterColor] = useState({}) 
+  function onTargetClick(outerColor, bonusPoints) {
+    setPoints(p => p+bonusPoints)
+    // if (clickedOuterColor[outerColor]) {
+    //   setClickedOuterColor(state => )
+    // }
+    const colorString = `${outerColor}`
+    setClickedOuterColor(state => ({...state, [outerColor]: `${outerColor}`}))
+    console.log(clickedOuterColor)
+  }
+
   return (
     <div>
-        <Target></Target>
+        {points ? `Current points ${points}`: `Click start to begin!`}
+        <div style={{marginLeft: '70vw'}}>
+        <Target onClick={onTargetClick}></Target>
+        </div>
     </div>
   )
 }
--- brainzone/src/components/AimTrainer/Target.js
+++ brainzone/src/components/AimTrainer/Target.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import './target.css'
-export const Target = () => {
+export const Target = ({onClick}) => {
+
+  const targetParts = [
+    {name: 'white', targetClass: "white-outer", points: 4},
+    {name: 'black', targetClass: "black-outer", points: 6},
+    {name: 'blue', targetClass: "blue-outer", points: 7},
+    {name: 'red', targetClass: "red-outer", points: 9},
+    {name: 'gold', targetClass: "gold-outer", points: 13},
+  ]
+  
   return (
     <div>
-        <div class="target">
-        <div class="white-outer"> </div>
-        <div class="black-outer"></div>
-        <div class="blue-outer"></div>
-        <div class="red-outer"></div>
-        <div class="gold-outer"></div>
-        <div class="gold-inner">
-  </div>
+        <div class="target" >
+        {targetParts.map((el) => {
+          console.log('pok')
+          return <div onClick={() => onClick(el.name, el.points)} className={el.targetClass} name={el.name}></div>
+        })}
+       
 </div>
     </div>
   )
