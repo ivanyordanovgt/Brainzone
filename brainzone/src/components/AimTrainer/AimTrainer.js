786ee2c6c926f459b083f51442adf18de4ae3ca4
Ivan <vankata294n4@gmail.com>
2023-04-24 16:05:40 +0300

    Added random placement of target

--- brainzone/src/components/AimTrainer/AimTrainer.js
+++ brainzone/src/components/AimTrainer/AimTrainer.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import { Target } from './Target'
 
 export const AimTrainer = () => {
-
+  const [random, setRandom] = useState({
+    'x': 100,
+    'y': 100,
+  })
   const [points, setPoints] = useState(false)
   const [clickedOuterColor, setClickedOuterColor] = useState({}) 
   function onTargetClick(outerColor, bonusPoints) {
     setPoints(p => p+bonusPoints)
-    // if (clickedOuterColor[outerColor]) {
-    //   setClickedOuterColor(state => )
-    // }
+    const [x, y] = [Math.floor(Math.random() * 1000), Math.floor(Math.random() * 600)]
+    setRandom(state => ({x: x, y: y}))
     if (clickedOuterColor[outerColor]) {
       setClickedOuterColor(state => ({...state, [outerColor]: clickedOuterColor[outerColor]+1 }))
     } else {
@@ -21,7 +23,7 @@ export const AimTrainer = () => {
   return (
     <div>
         {points ? `Current points ${points}`: `Click start to begin!`}
-        <div style={{marginLeft: '70vw'}}>
+        <div style={{marginLeft: random.x, marginTop: random.y}}>
         <Target onClick={onTargetClick}></Target>
         </div>
     </div>
