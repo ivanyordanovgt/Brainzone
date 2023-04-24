e4475416d21d1096fc6392beb80f52ecbadb72ce
Ivan <vankata294n4@gmail.com>
2023-04-24 15:16:45 +0300

    target adjustments

--- brainzone/src/components/AimTrainer/Target.js
+++ brainzone/src/components/AimTrainer/Target.js
@@ -5,16 +5,11 @@ export const Target = () => {
     <div>
         <div class="target">
         <div class="white-outer"> </div>
-        <div class="white-inner"></div>
         <div class="black-outer"></div>
-        <div class="black-inner"></div>
         <div class="blue-outer"></div>
-        <div class="blue-inner"></div>
         <div class="red-outer"></div>
-        <div class="red-inner"></div>
         <div class="gold-outer"></div>
         <div class="gold-inner">
-        <div class="cross"></div>
   </div>
 </div>
     </div>
