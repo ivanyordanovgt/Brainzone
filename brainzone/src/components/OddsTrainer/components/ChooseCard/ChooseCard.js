792576e460e9ab4c395aa372459ba81c2a38d5c9
Ivan <vankata294n4@gmail.com>
2023-04-27 22:04:15 +0300

    Added random card generation with custom amount of cards

--- brainzone/src/components/OddsTrainer/components/ChooseCard/ChooseCard.js
+++ brainzone/src/components/OddsTrainer/components/ChooseCard/ChooseCard.js
@@ -1,7 +1,48 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import styles from './ChooseCard.module.css'
+import image from '../../../../images/cardImages/Diamonds/3.png'
 export const ChooseCard = () => {
+  const cardTypes = {'red': ['Diamonds', 'Hearts'], 'black': ['Clubs', 'Spades']};
+  const [cards, setCards] = useState([{'url': randomCard('red'), 'state': 'no'}, {'url': randomCard('black'), 'state': 'no'}]);
+
+  function randomInt(min, max) {  
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  function generateCardOdds(cardsCount) {
+    const cardsStates = Array(cardsCount).fill('lose');
+    const cardsObjList = [];
+    const winningNumber = randomInt(0, cardsCount-1)
+    cardsStates[winningNumber] = 'win'
+    let color = 'black';
+    for (let cardState of cardsStates) {
+      color = color=='red' ? 'black': 'red';
+      let card = randomCard(color);
+      cardsObjList.push({'url': card, 'state': cardState});
+    }
+
+    setCards(cardsObjList)
+    return cardsObjList
+  }
+
+  function randomCard(color) {
+    const cardType = cardTypes[color][randomInt(0, 1)];
+    const cardNumber = randomInt(2, 10);
+    return `${cardType}/${cardNumber}.png`
+  }
+
   return (
-    <div>ChooseCard</div>
+    <div>
+      <div className={styles.cards}>
+        {cards.map((card) => {
+            return <img 
+            src={require(`../../../../images/cardImages/${card.url}`)}
+            win={card.state=='win' ? true: false}
+            ></img>
+        })}
+      <button onClick={() => generateCardOdds (2)}>Refresh</button>
+      </div>
+      
+    </div>
   )
 }
