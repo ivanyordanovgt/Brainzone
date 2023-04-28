de5ebe219032c10d1a805bbde1bfdff119f14421
Ivan <vankata294n4@gmail.com>
2023-04-28 14:34:34 +0300

    Card now generates when odds are chosen and when attempt to guess is made

--- brainzone/src/components/OddsTrainer/components/ChooseCard/ChooseCard.js
+++ brainzone/src/components/OddsTrainer/components/ChooseCard/ChooseCard.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react'
 import styles from './ChooseCard.module.css'
 import diamondImg from '../../../../images/cardImages/Diamonds/3.png'
-import ClubsImg from '../../../../images/cardImages/Clubs/J.png'
+import clubsImg from '../../../../images/cardImages/Clubs/J.png'
 import jokerImg from '../../../../images/cardImages/Jokers/Joker_1.png'
 import { OddsBox } from './OddsBox'
 export const ChooseCard = () => {
   const cardTypes = {'red': ['Diamonds', 'Hearts'], 'black': ['Clubs', 'Spades']};
+  const [oddsChoices, setOddsChoices] = useState([
+    {imgUrl: clubsImg, text:'50/50', cards: 2}, 
+    {imgUrl: diamondImg, text:'70/30', cards: 3}, 
+    {imgUrl: jokerImg, text: '20/80', cards: 4}])
+  const [cardsAmount, setCardsAmount] = useState(0)
   const [cards, setCards] = useState([]);
   const [message, setMessage] = useState('');
   function randomInt(min, max) {  
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
-  function generateCards(cardsCount) {
-    const cardsStates = Array(cardsCount).fill('lose');
+  function generateCards(cardsAmount) {
+    const cardsStates = Array(cardsAmount).fill('lose');
+    console.log(cardsAmount, '1111111111111111111111111')
     const cardsObjList = [];
-    const winningNumber = randomInt(0, cardsCount-1)
+    const winningNumber = randomInt(0, cardsAmount-1)
     cardsStates[winningNumber] = 'win'
     let color = 'black';
     for (let cardState of cardsStates) {
@@ -41,19 +47,26 @@ export const ChooseCard = () => {
     } else {
       setMessage("WRONG, GIVE ME UR MONEY ")
     }
+
+    generateCards(cardsAmount);
+  }
+
+  function onOddChoiceClick(amount) {
+    generateCards(amount)
+    setCardsAmount(amount)
   }
 
   return (
     <div className={styles.chooseCardMainDiv}>
       <h1>{message}</h1>
       <div className={styles.cards}>
-        {!cards.length ? <div>
+        {cards.length <= 0 ? <div>
           <h1>Welcome</h1>
           <h1>Choose odds</h1>
           <div className={styles.oddsChoice}>
-            <OddsBox cardUrl={diamondImg} text={'50/50'}/> 
-            <OddsBox cardUrl={ClubsImg} text={'30/70'}/> 
-            <OddsBox cardUrl={jokerImg} text={'20/80'}/> 
+            {oddsChoices.map((oddInfo) => {
+            return <OddsBox onClick={() => onOddChoiceClick(oddInfo.cards)} cardUrl={oddInfo.imgUrl} text={oddInfo.text}/> 
+            })}
           </div>
            
         </div>: ''}
@@ -64,7 +77,6 @@ export const ChooseCard = () => {
             id={card.state}
             ></img>
         })}
-      <button onClick={() => generateCards(3)}>Refresh</button>
       </div>
       
     </div>
--- brainzone/src/components/OddsTrainer/components/ChooseCard/OddsBox.js
+++ brainzone/src/components/OddsTrainer/components/ChooseCard/OddsBox.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styles from './ChooseCard.module.css'
-export const OddsBox = ({cardUrl, text}) => {
+export const OddsBox = ({cardUrl, text, onClick}) => {
   return (
-    <div className={styles.oddsBox}>
+    <div onClick={onClick} className={styles.oddsBox}>
         <img src={cardUrl}></img>
         <h1>{text}</h1>  
     </div> 
