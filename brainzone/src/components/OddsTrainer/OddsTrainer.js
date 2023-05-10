e43597107e5a99f52f4fc6a60db862f08df8e882
Ivan <vankata294n4@gmail.com>
2023-05-10 17:29:57 +0300

    added rocket animation with star background animation

--- brainzone/src/components/OddsTrainer/OddsTrainer.js
+++ brainzone/src/components/OddsTrainer/OddsTrainer.js
@@ -3,15 +3,32 @@ import { Link, Route, Routes } from 'react-router-dom';
 import { CardWar } from './components/CardWar/CardWar';
 import { ChooseCard } from './components/ChooseCard/ChooseCard';
 import { CrashGamble } from './components/CrashGamble/CrashGamble';
-
+import styles from './oddsTrainer.module.css';
+import cardImg from '../../images/cardImages/Jokers/Joker_2.png';
+import rocket from '../../images/crashGame/rocket.png'
 export const OddsTrainer = () => {
 
+
     return (
-        <div>
-            <h1>Can you beat the odds ?</h1>
-            <Link to="crash-gamble">Crash gamble</Link>
-            <Link to="choose-card">Choose card (50/50)</Link>
-            <Link to="card-war">Card war (custom odds)</Link>
-        </div>
+        <>
+        <div className={styles.rocketWrapper}>
+          <div className={styles.rocketDiv}>
+                    {[...Array(20).keys()].map(() => {
+                        let x = Math.floor(Math.random() * window.innerWidth*0.27)
+                        let h = Math.random() * 90 + 10;
+                        let duration = Math.random() * 1 + 0.2;
+                        return <i style={{
+                            left: `${x}px`,
+                            width: `1px`,
+                            height: `${h}px`,
+                            animationDuration: `${duration}s`
+                        }}></i>
+                    })}
+                    <div className={styles.rocket}>
+                        <img src={rocket}></img>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
