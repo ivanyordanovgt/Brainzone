23fd1da3330b199f728b9175fcfbb395b50acb88
Ivan <vankata294n4@gmail.com>
2023-05-10 18:45:34 +0300

    Fixed statement

--- brainzone/src/utils/cards.js
+++ brainzone/src/utils/cards.js
@@ -2,7 +2,7 @@ import { randomInt } from "./randomNumber";
 
 
 export function randomCard() {
-    const color = randomInt(0,1) % 2 ? 'red': 'black';
+    const color = randomInt(0,1) % 2 == 0 ? 'red': 'black';
     const cardTypes = {'red': ['Diamonds', 'Hearts'], 'black': ['Clubs', 'Spades']};
     const cardType = cardTypes[color][randomInt(0, 1)];
     const cardNumber = randomInt(2, 14);
